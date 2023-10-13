66bdb63c4d729f04190b6471a3b01e39397615c4
vv12138vv <jgss987611676@outlook.com>
2023-10-13 19:01:29 +0800

    about finish but maybe i need iterator

--- src/list.hpp
+++ src/list.hpp
@@ -8,6 +8,7 @@ Comment:Implement a List,i think need something following.
 
 
 #include <cstddef>
+#include <exception>
 #include <utility>
 namespace vvstl {
 using namespace std;
@@ -24,7 +25,7 @@ public:
         , next_(nullptr)
         , pre_(nullptr) {}
     explicit ListNode(T&& data)
-        : data_(data)
+        : data_(std::move(data))
         , next_(nullptr)
         , pre_(nullptr) {}
     explicit ListNode(const ListNode<T>& that) {
@@ -37,6 +38,7 @@ public:
         next_ = that.next_;
         pre_ = that.pre_;
     }
+    virtual ~ListNode() {}
     explicit ListNode(ListNode<T>&& that) {
         data_ = std::move(that.data_);
         next_ = std::move(that.next_);
@@ -59,27 +61,37 @@ class List {
 private:
     ListNode<T>* head_ = nullptr;
     ListNode<T>* tail_ = nullptr;
+    size_t size_ = 0;   // according to stl,use it for O(1)
 
 public:
     explicit List() noexcept
         : head_(nullptr)
-        , tail_(nullptr) {}
+        , tail_(nullptr)
+        , size_(0) {}
 
-    virtual ~List() noexcept {}
+    virtual ~List() noexcept {
+        ListNode<T>* cur = head_;
+        while (cur != tail_ && cur != nullptr) {
+            ListNode<T>* next = cur->next_;
+            delete cur;
+            cur = next;
+        }
+        delete cur;
+        head_ = nullptr;
+        tail_ = nullptr;
+    }
     explicit List(const List<T>& that) {}
     explicit List(List<T>&& that) {}
-    List& operator=(const List<T>& that){
-        
-    }
-    List& operator=(List<T>&& that){
+    List& operator=(const List<T>& that) {}
+    List& operator=(List<T>&& that) {}
 
-    }
     void push_back(const T& data) {
         if (head_ == nullptr) {
             head_ = new ListNode<T>(data);
             tail_ = head_;
             head_->next_ = head_;
             head_->pre_ = head_;
+            size_ += 1;
             return;
         }
         ListNode<T>* temp = new ListNode<T>(data);
@@ -88,6 +100,7 @@ public:
         tail_ = temp;
         tail_->next_ = head_;
         head_->pre_ = tail_;
+        size_ += 1;
     }
     void push_back(T&& data) {
         if (head_ == nullptr) {
@@ -95,6 +108,7 @@ public:
             tail_ = head_;
             head_->next_ = head_;
             head_->pre_ = head_;
+            size_ += 1;
             return;
         }
         ListNode<T>* temp = new ListNode<T>(std::move(data));
@@ -103,6 +117,7 @@ public:
         tail_ = temp;
         tail_->next_ = head_;
         head_->pre_ = tail_;
+        size_ += 1;
     }
     void push_front(const T& data) {
         if (head_ == nullptr) {
@@ -110,6 +125,7 @@ public:
             tail_ = head_;
             head_->next_ = head_;
             head_->pre_ = head_;
+            size_ += 1;
             return;
         }
         ListNode<T>* temp = new ListNode<T>(data);
@@ -118,6 +134,7 @@ public:
         temp->pre_ = tail_;
         tail_->next_ = temp;
         head_ = temp;
+        size_ += 1;
     }
     void push_front(T&& data) {
         if (head_ == nullptr) {
@@ -125,6 +142,7 @@ public:
             tail_ = head_;
             head_->next_ = head_;
             head_->pre_ = head_;
+            size_ += 1;
             return;
         }
         ListNode<T>* temp = new ListNode<T>(std::move(data));
@@ -133,21 +151,98 @@ public:
         temp->pre_ = tail_;
         tail_->next_ = temp;
         head_ = temp;
+        size_ += 1;
     }
-    bool empty() {
-        return head_==nullptr;
-    }
-    size_t size() {
-        ListNode<T>* cur = head_;
-        if (cur == nullptr) {
-            return 0;
+    void pop_back() {
+        if (tail_ == nullptr) {
+            return;
         }
-        size_t res = 0;
-        do {
-            res += 1;
-            cur = cur->next_;
-        } while (cur != head_);
-        return res;
+        if (head_ == tail_) {
+            // tail_->data_.~T();//no need to call the destructor explicityly
+            delete tail_;   // when delete, it will be called automatically
+            head_ = nullptr;
+            tail_ = nullptr;
+            size_ -= 1;
+            return;
+        }
+        ListNode<T>* temp = tail_->pre_;
+        // tail_->data_.~T();
+        temp->next_ = tail_->next_;
+        head_->pre_ = temp;
+        delete tail_;
+        size_ -= 1;
+        tail_ = temp;
     }
-};
-}   // namespace vvstl
+    void pop_front() {
+        if (head_ == nullptr) {
+            return;
+        }
+        if (head_ == tail_) {
+            delete head_;
+            head_ = nullptr;
+            tail_ = nullptr;
+            size_ -= 1;
+            return;
+        }
+        ListNode<T>* temp = head_->next_;
+        tail_->next_ = temp;
+        temp->pre_ = tail_;
+        delete head_;
+        size_ -= 1;
+        head_ = temp;
+    }
+    bool empty() { return head_ == nullptr; }
+    size_t size() const { return size_; }
+    T& operator[](size_t index) {
+        if (index > size_ - 1 || index < 0) {
+            return tail_->data_;
+        }
+        int count = 0;
+        ListNode<T>* cur = head_;
+        while (count != index) {
+            cur = cur->next_;
+            count += 1;
+        }
+        return cur->data_;
+    }
+    T& front() { return head_->data_; }
+    T& back() { return tail_->data_; }
+    void insert(const T& data, size_t index) {
+        if (index > size_ - 1 || index < 0) {
+            return;
+        }
+        ListNode<T>* cur = head_;
+        size_t count = 0;
+        while (count != index) {
+            cur = cur->next_;
+            count += 1;
+        }
+        ListNode<T>* pre = cur->pre_;
+        ListNode<T>* temp = new ListNode<T>(data);
+        pre->next_ = temp;
+        temp->pre_ = pre;
+        temp->next_ = cur;
+        cur->pre_ = temp;
+        size_ += 1;
+    }
+    void insert(T&& data, size_t index) {
+        if (index > size_ - 1 || index < 0) {
+            return;
+        }
+        ListNode<T>* cur = head_;
+        size_t count = 0;
+        while (count != index) {
+            cur = cur->next_;
+            count += 1;
+        }
+        ListNode<T>* pre = cur->pre_;
+        ListNode<T>* temp = new ListNode<T>(std::move(data));
+        pre->next_ = temp;
+        temp->pre_ = pre;
+        temp->next_ = cur;
+        cur->pre_ = temp;
+        size_ += 1;
+    }
+
+};   // namespace vvstl
+}
--- src/list_test.cpp
+++ src/list_test.cpp
@@ -1,10 +1,70 @@
-#include<iostream>
+#include "list.hpp"
+#include <iostream>
 #include <utility>
 
-int main(){
-    int * p=new int(1);
-    int * m;
-    m=std::move(p);
-    std::cout<<p<<"\n";
-    std::cout<<m<<"\n";
+using namespace vvstl;
+
+
+class Object {
+private:
+    int* a_ = nullptr;
+
+public:
+    Object(int value = 0)
+        : a_(new int[3]) {
+        for (int i = 0; i < 3; i++) {
+            a_[i] = value;
+        }
+    }
+    Object(const Object& that) {
+        delete[] a_;
+        a_ = new int[3];
+        for (int i = 0; i < 3; i++) {
+            a_[i] = that.a_[i];
+        }
+    }
+    Object(Object&& that) {
+        delete [] a_;
+        a_ = that.a_;
+        that.a_ = nullptr;
+    }
+    Object& operator=(Object&& that) {
+        delete [] a_;
+        a_ = that.a_;
+        that.a_ = nullptr;
+        return *this;
+    }
+    Object& operator=(const Object& that) {
+        delete[] a_;
+        a_ = new int[3];
+        for (int i = 0; i < 3; i++) {
+            a_[i] = that.a_[i];
+        }
+        return *this;
+    }
+    void print() { std::cout << "Object" << a_[0] << "\n"; }
+    ~Object() {
+        std::cout<<"Object Destruct:"<<a_[0]<<"\n";
+        delete[] a_;
+        a_ = nullptr;
+    }
+};
+
+
+int main() {
+    List<Object> list;
+    Object o1(1);
+    Object o2(2);
+    Object o3(3);
+    std::cout << list.empty() << "\n";
+    std::cout << list.size() << "\n";
+    list.push_back(std::move(o1));
+    list.push_back(std::move(o2));
+    list.push_front(std::move(o3));
+    Object o4(4);
+    list.insert(std::move(o4),1);
+    for(size_t i=0;i<list.size();i++){
+        list[i].print();
+    }
+    return 0;
 }
